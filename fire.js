8487eadcbed7f66518a73b6cacff9d2a581ff4e6
Najm Sheikh <najm97@gmail.com>
2015-06-01 17:20:26 -0400

    Server backend code

--- fire.js
+++
@@ -1,9 +0,0 @@
-var firebase = require('firebase');
-var fb = new firebase('https://proficient.firebaseio.com/Mathematics');
-
-fb.on('value', function(datasnap) {
-    datasnap.forEach(function(snapshot) {
-    	var name = snapshot.child('name').val();
-        console.log(name);
-    })
-})
